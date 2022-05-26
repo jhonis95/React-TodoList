6753bc3992f38c2e0f1c464a1f24a0ed945d3433
jonatan de oliveira <j.jhonis95@gmail.com>
2022-05-26 11:48:02 -0400

    finishing style

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,6 +1,7 @@
 import React,{Component} from 'react'
 import Todo from './Todo'
 import NewTodoForm from './NewTodoForm'
+import './TodoList.css'
 
 class TodoList extends Component{
     constructor(props){
@@ -61,12 +62,14 @@ class TodoList extends Component{
             )
         })
         return(
-            <div>
-                <h1>Todo List</h1>
-                <NewTodoForm createTodo={this.create}/>
+            <div className='TodoList'>
+                <h1>
+                    Todo List<span>A Simple React Todo List App.</span>
+                </h1>
                 <ul>
                     {todos}
                 </ul>
+                <NewTodoForm createTodo={this.create}/>
             </div>
         )
     }
